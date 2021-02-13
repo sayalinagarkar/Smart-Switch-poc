f535add87e675f5e22b686a9cd8981b7e7bb3cfe
unknown <yrk9421@gmail.com>
2021-02-13 23:35:59 +0530

    Infinite swiped tabs feature

--- src/pages/room-switch-container/room-switch-container.ts
+++ src/pages/room-switch-container/room-switch-container.ts
@@ -27,6 +27,7 @@ import { RootedIpInputModelPage } from "../rooted-ip-input-model/rooted-ip-input
 })
 export class RoomSwitchContainerPage {
   @ViewChild("slider") slider: Slides;
+  @ViewChild("segments") segments;
   deviceListConfiguration;
   totalDeviceStatus = [];
   pages1: Array<{ title: string; component: any }>;
@@ -73,6 +74,11 @@ export class RoomSwitchContainerPage {
     this.initDeviceListConfiguration();
 
   }
+  // Initialize slider
+  ionViewDidEnter(){
+    this.slideChanged();
+  }
+
   addRootedIP(){
     this.navCtrl.push(RootedIpInputModelPage);
 
@@ -145,6 +151,69 @@ export class RoomSwitchContainerPage {
     this.slider.slideTo(index);
     this.roomName=this.rooms[Number(index)];
   }
+  // On slide changed
+  slideChanged() {
+    let currentIndex = this.slider.getActiveIndex();
+    let slides_count = this.segments.nativeElement.childElementCount;
+
+    this.pages = currentIndex.toString();
+    if(this.pages >= slides_count)
+      this.pages = (slides_count-1).toString();
+
+    this.roomName=this.rooms[Number(this.pages)];
+    this.switchBoardList = this.roomDetailsProvider.getSwitchBoardList(
+      Number(this.pages)
+    );
+
+    this.sendRoomChangedData();
+    this.centerScroll();
+  }
+
+  // Center current scroll
+  centerScroll(){
+    if(!this.segments || !this.segments.nativeElement)
+      return;
+
+    let sizeLeft = this.sizeLeft();
+    let sizeCurrent = this.segments.nativeElement.children[this.pages].clientWidth;
+    let result = sizeLeft - (window.innerWidth / 2) + (sizeCurrent/2) ;
+
+    result = (result > 0) ? result : 0;
+    this.smoothScrollTo(result);
+  }
+
+  // Get size start to current
+  sizeLeft(){
+    let size = 0;
+    for(let i = 0; i < Number(this.pages); i++){
+      size+= this.segments.nativeElement.children[i].clientWidth;
+    }
+    return size;
+  }
+
+  // Easing function
+  easeInOutQuart(time, from, distance, duration) {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  }
+
+  // Animate scroll
+  smoothScrollTo(endX){
+    let startTime = new Date().getTime();
+    let startX = this.segments.nativeElement.scrollLeft;
+    let distanceX = endX - startX;
+    let duration = 200;
+
+    let timer = setInterval(() => {
+      var time = new Date().getTime() - startTime;
+      var newX = this.easeInOutQuart(time, startX, distanceX, duration);
+      if (time >= duration) {
+        clearInterval(timer);
+      }
+      this.segments.nativeElement.scrollLeft = newX;
+    }, 1000 / 60); // 60 fps
+  }
+
   moveSlider($event) {
     this.pages = this.slider.getActiveIndex().toString();
     this.roomName=this.rooms[Number(this.pages)];
